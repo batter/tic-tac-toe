dcd6858861300bc56dabcb7541babeb8d92d92ec
Ben Atkins <batkinz@gmail.com>
2016-12-14 22:58:04 -0500

    Setup more basic tests for the Game model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Game, type: :model do
+  let(:game) { Game.create }
+
   it { expect(Game.included_modules).to include(Mongoid::Document) }
+
+  describe "Associations" do
+    describe :players do
+      it { expect(Game.embedded_relations.keys).to include('players') }
+
+      describe :find_by_symbol do
+        let!(:player) { game.add_player!('Ben') }
+
+        it "should locate a player via it's symbol" do
+          expect(game.players.find_by_symbol(player.symbol)).to eq(player)
+        end
+
+        it "should not raise if no matching player is found" do
+          expect(game.players.find_by_symbol('Z')).to be_nil
+        end
+      end
+    end
+  end
 end
